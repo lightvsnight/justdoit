b1eb08aaff1d207ba918893625c511d095c3d5af
lihongkun <ziyou1102@aliyun.com>
2024-12-04 23:00:26 +0800

    第一次提交

--- authserver/src/main/java/org/example/ServerApplication.java
+++ authserver/src/main/java/org/example/ServerApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableAutoConfiguration
 //@AutoConfigurationPackage
 @SpringBootApplication
-public class Main {
+public class ServerApplication {
     public static void main(String[] args) {
         System.out.println("Hello world! AuthServer Application");
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 }
