3e89980fa50422a67d75baf35fc82990c9bc4247
lihongkun <ziyou1102@aliyun.com>
2025-12-19 21:26:35 +0800

    改造

---
+++ authserver/main/src/main/java/com/sample/modules/tAuthorizationConsent/service/impl/TAuthorizationConsentServiceImpl.java
@@ -0,0 +1,37 @@
+//package com.sample.modules.tAuthorizationConsent.service.impl;
+//
+//
+//import com.baomidou.mybatisplus.extension.service.IService;
+//import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+//import com.sample.modules.tAuthorizationConsent.entity.TAuthorizationConsent;
+//import com.sample.modules.tAuthorizationConsent.mapper.TAuthorizationConsentMapper;
+//import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
+//import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * <p>
+// *  服务实现类
+// * </p>
+// *
+// * @author Li.HongKun
+// * @since 2025-11-30 20:00:23
+// */
+//@Service
+//public class TAuthorizationConsentServiceImpl extends ServiceImpl<TAuthorizationConsentMapper, TAuthorizationConsent> implements IService<TAuthorizationConsent>, OAuth2AuthorizationConsentService {
+//
+//    @Override
+//    public void save(OAuth2AuthorizationConsent authorizationConsent) {
+//
+//    }
+//
+//    @Override
+//    public void remove(OAuth2AuthorizationConsent authorizationConsent) {
+//
+//    }
+//
+//    @Override
+//    public OAuth2AuthorizationConsent findById(String registeredClientId, String principalName) {
+//        return null;
+//    }
+//}
